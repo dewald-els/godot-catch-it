6d7ecc6dd0958ce3c03013ce244437daf7262d87
MrBetaMax <dewaldifels@gmail.com>
2023-03-11 12:56:46 +0100

    feat: Max Bomb capacity for boxes

--- Scenes/BombDropOff/BombDropOff.cs
+++ Scenes/BombDropOff/BombDropOff.cs
@@ -4,22 +4,39 @@ using System;
 public partial class BombDropOff : Node2D
 {
     private Area2D BombDropOffArea;
+    private TextureProgressBar CapacityBar;
     private AnimationPlayer AnimationPlayer;
+    [Export]
+    private int BombCapacity = 1;
+    private int BombCount = 0;
 
     public override void _Ready()
     {
+        CapacityBar = GetNode<TextureProgressBar>("CapacityBar");
         AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
         BombDropOffArea = GetNode<Area2D>("BombDropOffArea");
         // Events
         BombDropOffArea.Connect("body_entered", new Callable(this, "OnBombDropOffAreaEntered"));
     }
 
+    private Boolean IsFull()
+    {
+        return BombCount >= BombCapacity;
+    }
+
+    public override void _Process(double delta)
+    {
+
+    }
+
     public void OnBombDropOffAreaEntered(Node2D body)
     {
-        if (body is Player player)
+        if (!IsFull() && body is Player player)
         {
             SignalBus.Instance.EmitSignal("PlayerDroppedBomb");
             AnimationPlayer.Play("BombDropped");
+            BombCount++;
+            CapacityBar.Value = Mathf.Ceil((float)BombCount / BombCapacity * 100);
         }
     }
 }
