be17bbd5c72c2cc0411824bc7119bca3eed8231b
MrBetaMax <dewaldifels@gmail.com>
2023-04-25 21:44:18 +0200

    feat: Toggle BombSpawner Enabled/Disabled

--- Scenes/Main/Main.cs
+++ Scenes/Main/Main.cs
@@ -4,18 +4,17 @@ using System;
 public partial class Main : Node2D
 {
 
-    [Export] private PackedScene[] Scenes;
+	[Export] private PackedScene[] Scenes;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        RenderingServer.SetDefaultClearColor(new Color(63.0f, 56.0f, 81.0f, 1.0f));
-        LevelManager.Instance.SetScenes(Scenes);
-        LevelManager.Instance.LoadLevel(0);
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		LevelManager.Instance.SetScenes(Scenes);
+		LevelManager.Instance.LoadLevel(0);
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
--- Scenes/World/World.cs
+++ Scenes/World/World.cs
@@ -11,7 +11,21 @@ public partial class World : Node2D
     public override void _Ready()
     {
         markers = GetTree().GetNodesInGroup("BombSpawnMarker");
-        GD.Print("Markers: " + markers.Count);
+
+        for (int i = 0; i < markers.Count; i++)
+        {
+            var marker = markers[i] as BombSpawnPoint;
+
+            if (marker.Disabled == true)
+            {
+                GD.Print("removing marker " + i);
+                markers.Remove(marker);
+            }
+        }
+
+        GD.Print("Markers", markers);
+        GD.Print("found " + markers.Count + " BombSpawnPoints");
+
         BombSpwawnTimer = GetNode<Timer>("BombSpawnTimer");
         BombSpwawnTimer.Timeout += OnBombSpwawnTimerTimeout;
         SignalBus.Instance.Connect("BombDropOffFull", new Callable(this, "OnBombDropOffFull"));
@@ -26,16 +40,11 @@ public partial class World : Node2D
     {
         var bomb = scene.Instantiate() as Bomb;
         AddChild(bomb);
-        var marker = markers[random.RandiRange(0, markers.Count - 1)] as Marker2D;
+        var marker = markers.PickRandom() as Marker2D; //markers[random.RandiRange(0, markers.Count - 1)] as Marker2D;
         bomb.Position = marker.Position;
 
         BombSpwawnTimer.Stop();
         BombSpwawnTimer.Start();
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
     }
 
     public override void _ExitTree()
